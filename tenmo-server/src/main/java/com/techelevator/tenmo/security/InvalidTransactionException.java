a488a9b5220677bb9e90b9cfac46cb9f8a1ae576
Robyn Florentz <r.a.florentz@gmail.com>
2022-07-08 15:50:56 -0400

    capstone progress Friday 2

--- tenmo-server/src/main/java/com/techelevator/tenmo/security/InvalidTransactionException.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/security/InvalidTransactionException.java
@@ -3,13 +3,13 @@ package com.techelevator.tenmo.security;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Invalid transaction, check IDs.")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Invalid transaction, check recipient username.")
 public class InvalidTransactionException extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
 
     public InvalidTransactionException(){
-        super("Invalid transaction, check IDs.");
+        super("Invalid transaction, check recipient username.");
     }
 
 }
